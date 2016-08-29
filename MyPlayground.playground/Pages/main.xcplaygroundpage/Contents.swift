e7cbe80a4c8349dbf4a12d3fc0d9ae1d06844940
Ben Su <bensu1013@hotmail.com>
2016-08-29 18:09:57 -0400

    Hi

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -16,85 +16,109 @@
 /*: Question 1
 ### 1. Add two Doubles
 */
-// write your code here
+func addDoubles(num1: Double, num2: Double) -> Double {
+    return (num1 + num2)
+}
 
 
 /*: Question 2
 ### 2. Add an Int to a Double
 */
-// write your code here
+func addMixed(num1: Int, num2: Double) -> Double {
+    let changeDouble = Double(num1)
+    return (changeDouble + num2)
+}
 
 
 /*: Question 3
 ### 3. Compare two Ints for equality
 */
-// write your code here
+func equalI(num1: Int, num2: Int) -> Bool {
+    return (num1 == num2)
+}
 
 
 /*: Question 4
 ### 4. Compare two Doubles for equality
 */
-// write your code here
+func equalD(num1: Double, num2: Double) -> Bool {
+    return (num1 == num2)
+}
 
 
 /*: Question 5
 ### 5. Compare an Int and a Double equality
 */
-// write your code here
+func equalMixed(num1: Int, num2: Double) -> Bool {
+    let changeNum = Double(num1)
+    return (changeNum == num2)
+}
 
 
 /*: Question 6
 ### 6. Compare 3 and 3.5 for equality (NB what do you have to be mindful of in this case?)
 */
-// write your code here
+(3 == 3.5)
 
 
 /*: Question 7
 ### 7. Compare 3 and 3.5 for equality and 6.1 and 7 for inequality
 */
-// write your code here
+(3 == 3.5) && (6.1 != 7)
 
 
 /*: Question 8
 ### 8. Write code expressing "if a less than b or x is greater than or equal to y"
 */
-// write your code here
+var a = 5
+var b = 17
+var x = 6.1
+var y = 3.6
+
+(a < b) || (x >= y)
+
 
 
 /*: Question 9
 ### 9. Write a function which returns the sum of three Ints
 */
-// write your code here
+func sumThree(num1: Int, num2: Int, num3: Int) -> Int {
+    return (num1 + num2 + num3)
+}
 
 
 /*: Question 10
 ### 10. Write a function average_i which returns the average of three Ints as an Int
 */
-// write your code here
-
+func average_i(num1: Int, num2: Int, num3: Int) -> Int {
+    return ((num1 + num2 + num3) / 3)
+}
+average_i(4, num2: 17, num3: 11)
 
 /*: Question 11
 ### 11. Use the return value of the function written for Question 10 in a boolean expression
 */
-// write your code here
+(average_i(10, num2: 15, num3: 429) > 200)
 
 
 /*: Question 12
 ### 12. Write a function average_f which returns the average of three Ints as a Float
 */
-// write your code here
+func average_f(num1: Int, num2: Int, num3: Int) -> Float {
+    return(Float(num1 + num2 + num3) / 3)
+}
 
 
 /*: Question 13
 ### 13. Call average_f with values 1, 3, and 5 and test if the result equals 3.0
 */
-// write your code here
+average_f(1, num2: 3, num3: 5) == 3.0
 
 
 /*: Question 14
 ### 14. Call average_i with values 1, 3, and 5 and test if the result is both greater than 1 and less than 5.
 */
-// write your code here
+(average_i(1, num2: 3, num3: 5) > 1) && (average_i(1, num2: 3, num3: 5) < 5)
 
 
 
